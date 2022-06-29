11459a94293df0eb8352686419fe5c2cf40a7753
Jacob Manley <jbmanley45@gmail.com>
2022-06-29 10:44:19 -0700

    completed

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,23 +1,51 @@
 import React, { useState, useEffect } from "react";
 import ListingCard from "./ListingCard"
+import NewListingForm from './NewListingForm';
 
 
-function ListingsContainer() {
-  const [listings, setListings] = useState([])
+function ListingsContainer({ search }) {
+  const [listings, setListings] = useState([]);
+  const [sortBy, setSortBy] = useState("id");
 
   useEffect(() => {
     fetch("http://localhost:6001/listings")
     .then((r) => r.json())
     .then(listings => setListings(listings))
   }, [])
-  console.log(listings)
-  const listingsCards = listings.map(listingObj => {
-    return <ListingCard key={listingObj.id} listing={listingObj}/>
+
+  function handleDeleteListing(id){
+    const updatedListingsArray = listings.filter(listing => listing.id !== id)
+    setListings(updatedListingsArray)
+  }
+
+  function handleAddListing(newListing) {
+    const updatedListingsArray = [newListing, ...listings]
+    setListings(updatedListingsArray)
+  }
+  const listingsCards = listings
+  .filter(listing => {
+    return listing.description.toLowerCase().includes(search.toLowerCase());
+  })
+  .sort((listingA, listingB) => {
+    if (sortBy === "id"){
+      return listingA.id - listingB.id;
+    } else {
+      return listingA.location.localeCompare(listingB.location)
+    }
+  })
+  .map((listingObj) => {
+    return <ListingCard 
+    key={listingObj.id} 
+    listing={listingObj}
+    onDeleteListing={handleDeleteListing}
+    />
   })
   return (
     <main>
-      <ul className="cards">{listingsCards}
-      </ul>
+      <NewListingForm onAddListing={handleAddListing}/>
+      <button onClick={() => setSortBy("id")}>Sort By Default</button>
+      <button onClick={() => setSortBy("location")}>Sort By Location</button>
+      <ul className="cards">{listingsCards}</ul>
     </main>
   );
 }
